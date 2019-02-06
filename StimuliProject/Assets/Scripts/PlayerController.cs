6ce4a3e4cca879c35ce1390b0a8292ce0568799f
Jake Walder <jakerzw@hotmail.co.uk>
2019-02-06 13:20:32 +0000

    Targets functioning

---
+++ StimuliProject/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (Input.GetMouseButtonDown(0)) 
+		{
+            Debug.Log ("Mouse button clicked");
+
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit2D hit = Physics2D.GetRayIntersection(ray, 100);
+
+            //Debug.Log("Hit Target");
+            if (hit.collider != null)
+            {
+                Debug.Log(hit.collider.gameObject.tag);
+                Destroy(hit.collider.gameObject);
+            }      
+		}
+	}
+}
---
+++ StimuliProject/Assets/Scripts/TargetController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void Hit()
+	{
+		Destroy (this.gameObject);
+	}
+}
