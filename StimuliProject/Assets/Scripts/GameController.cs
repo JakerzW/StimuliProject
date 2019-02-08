8dcb9c72feb87475c5e2d852633b84a8f8d02593
Jake Walder <jakerzw@hotmail.co.uk>
2019-02-08 17:31:12 +0000

    Countdown timer implemented

--- StimuliProject/Assets/Scripts/GameController.cs
+++ StimuliProject/Assets/Scripts/GameController.cs
@@ -5,36 +5,102 @@ using UnityEngine.UI;
 
 public class GameController : MonoBehaviour
 {
+    enum GameState {start, wait, spawn, end};
+    GameState currentState;
+    
+    float currentTime;
+    int currentCount;
 
     public int score;
     public Text scoreText;
+    public Text countdownText;
+
+    public int maxTargets;
 
     void Awake()
     {
         score = 0;
+        currentState = GameState.start;
+        currentTime = 0.0f;
+        currentCount = 3;
+
+        countdownText.enabled = true;
+        scoreText.enabled = false;
     }
 
     // Use this for initialization
     void Start ()
     {
-		
+        countdownText.text = currentCount.ToString();
 	}
 	
 	// Update is called once per frame
 	void Update ()
     {
-        if (scoreText == null)
+        if (currentState == GameState.start)
         {
-            Debug.Log("Score Text is null.");
+            currentTime += Time.deltaTime;
+
+            if (currentTime > 1 && currentTime < 2)
+            {
+                currentCount = 2;
+            }
+            else if (currentTime > 2 && currentTime < 3)
+            {
+                currentCount = 1;
+            }
+            else if (currentTime > 3 && currentTime < 4)
+            {
+                currentCount = 0;
+            }
+            else if (currentTime > 4)
+            {
+                countdownText.enabled = false;
+                scoreText.enabled = true;
+                currentState = GameState.spawn;
+            }         
         }
-        else
+
+        if (currentState == GameState.wait || currentState == GameState.spawn)
         {
-            scoreText.text = "Score: " + score;
+            updateTargets();
         }
+    }
+
+    void OnGUI()
+    {
+        if (currentState == GameState.start)
+        {
+            if (currentCount > 0)
+            {
+                countdownText.text = currentCount.ToString();
+            }
+            else if (currentCount == 0)
+            {
+                countdownText.text = "Go!";
+            }
+        }
+        
+        if (currentState == GameState.wait || currentState == GameState.spawn)
+        {
+            if (scoreText == null)
+            {
+                Debug.Log("Score Text is null.");
+            }
+            else
+            {
+                scoreText.text = "Score: " + score;
+            }
+        }        
     }
 
     public void IncrementScore()
     {
         score++;
+    }
+
+    void updateTargets()
+    {
+
     }
 }
